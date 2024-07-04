2ce823abf96a585a73d3d84e515fe72b93cdc8f7
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-07-04 22:00:34 +0530

    day-4

---
+++ Javascript/day-14/main.js
@@ -0,0 +1,138 @@
+// Operators :- 
+
+// 1. Arithmetic Operators
+
+
+// let Number1 = 3;
+// let Number2 = 3;
+
+
+// Increment (++)
+// Number1++; 
+// ++Number1;
+// console.log(Number1);
+
+// Decrement (--)
+// Number2--;
+// --Number2;
+// console.log(Number2)
+
+
+
+// Exponentiation (**)
+// console.log(Number1 ** Number2);
+
+
+// Modulus (%)
+// console.log(Number2 % Number1) 
+
+// Division (/)
+
+// console.log(Number2 / Number1) 
+
+// Multiplication (*)
+// console.log(Number1 * Number2)
+
+
+// Subtraction (-)
+
+// console.log(Number2 - Number1);
+
+
+// Addition (+)
+// console.log(Number1 + Number2);
+
+
+
+
+
+
+
+
+// 2. Comparison Operators
+
+// - Equal to (==)
+
+
+// console.log(a==b);
+
+// - Not equal to (!=)
+
+// console.log(a != b );
+
+
+
+
+// - Strict equal to (===) (values and data types are equal)
+
+// console.log( a===b);
+
+
+
+
+// - Strict not equal to (!==)  // Assignment
+
+
+
+// - Greater than (>)
+// console.log(a > b);
+
+// - Less than (<)
+// console.log(a < b);
+
+// let a = 0;
+// let b = 10;
+// - Greater than or equal to (>=)
+
+// console.log(a >= b);
+// - Less than or equal to (<=) -- Assignment
+
+
+// 3. Logical Operators (true and false)
+
+// - Logical AND (&&)
+
+// console.log( true && false) //false;
+// console.log(false && true) // false;
+// console.log(true && true)  // true;
+// console.log(false && false) 
+
+// - Logical OR (||)
+
+// console.log( true || false)
+// console.log(false || true)
+// console.log(true || true);
+// console.log(false || false)
+
+// - Logical NOT (!)
+console.log(!true)
+console.log(!false)
+
+
+// 4. Assignment Operators
+
+// - Assignment (=)
+let Name = "Amit";
+// - Addition Assignment (+=)
+let Marks = 99;
+Marks += 1;
+console.log(Marks);
+
+// - Subtraction Assignment (-=)
+// - Multiplication Assignment (*=)
+// - Division Assignment (/=)
+
+
+
+//? 5. Conditional (Ternary) Operator
+//! 6. Bitwise Operators
+//! 7. Special Operators
+
+
+
+
+// typeOf 
+
+let ex = true;
+
+console.log( typeof ex);
