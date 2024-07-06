577c1cc3fd8d15929dda6694f32ef43692f6b734
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-07-06 21:38:43 +0530

    day-16

---
+++ Javascript/day-15/string_method.js
@@ -0,0 +1,66 @@
+// let myString = "suraj"
+
+// console.log( myString.length)
+
+// myString[0] = "hs"
+
+// console.log( myString)
+
+// console.log( myString[0])
+
+
+// *2. indexOf() Method
+
+// let greeting = "Hello, World!";
+// // console.log(greeting.length); // 13
+
+// console.log(greeting.indexOf("a"))
+
+
+// *3. Slice()
+
+// let greeting = "Hello, World!";
+
+// let newString = greeting.slice(0)
+// console.log(newString) // "Wor"
+
+
+
+// let world = greeting.slice(7, 12);
+// console.log(world); // "World"
+
+
+// *4. replace();
+
+// let greeting = "Hello, world! world , world , world";
+
+// let newString = greeting.replace("world", "Universe");  
+
+// console.log(newString)
+
+
+
+
+
+// let newGreeting = greeting.replace("World", "Universe");
+// console.log(newGreeting); // "Hello, Universe!"
+
+
+
+// *5 Split() Method
+// let greeting = "Hello World!";
+
+// let newString = greeting.split(" ");
+
+// console.log(newString) // ["HelloWorld!"]
+
+// *6 join() Method
+let words = ["Hello", "World",];
+
+let greeting = words.join("@")
+
+console.log(greeting) // "HelloWorld"
+
+// let greeting = words.join(", ");
+// console.log(greeting); // "Hello, World!"
+
