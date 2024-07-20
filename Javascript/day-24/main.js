ebd1546433cd63106c942c38c93b417a321e4f4c
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-07-20 20:00:20 +0530

    project

---
+++ Javascript/day-24/main.js
@@ -0,0 +1,23 @@
+// // localStorage.setItem("username" , "Suraj");
+// // // // localStorage.setItem("username" , "Rahul");
+// // localStorage.setItem("email" , "suraj@google.com");
+
+// // // console.log(localStorage.getItem("username"));
+
+// // // localStorage.removeItem("username");
+
+
+// // localStorage.clear();
+
+
+// // 1.store✅ , 2. get✅  , 3. ✅remove , 4. clear , 5.✅ update
+
+
+
+// // sessionStorage.setItem("username","Suraj");
+
+// // console.log(sessionStorage.getItem("username"));
+
+// sessionStorage.removeItem("username");
+
+// sessionStorage.clear();
